15305734e02377df0ed751e5f6e2dd94f7241a73
sowmya reddy <atlasowmya14@gmail.com>
2021-07-16 22:53:47 -0500

    added a new UIViewController

--- MovieFlix/MovieFlix/ViewController.swift
+++ MovieFlix/MovieFlix/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         tableView.delegate = self
         // Do any additional setup after loading the view.
         
-        print("Hello")
+        //print("Hello")
         
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
@@ -38,7 +38,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
                 self.movies = dataDictionary["results"] as!  [[String:Any]]
                 self.tableView.reloadData()
                 
-                print(dataDictionary)
+                //print(dataDictionary)
 
                     // TODO: Get the array of movies
                     // TODO: Store the movies in a property to use elsewhere
@@ -69,6 +69,24 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         cell.posterView.af_setImage(withURL: posterUrl!)
         return cell
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        print("Loading up the details screen")
+        
+        //Find the selected movie
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        
     }
 
 
